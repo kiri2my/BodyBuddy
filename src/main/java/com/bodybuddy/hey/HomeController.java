c70b62ffc60f55fef5300224c9db1bda82ff0dbd
steveKo <junggun24@naver.com>
2019-05-31 16:04:24 +0900

    han sol

--- src/main/java/com/bodybuddy/hey/HomeController.java
+++ src/main/java/com/bodybuddy/hey/HomeController.java
@@ -32,28 +32,5 @@ public class HomeController {
 		
 		return "main";
 	}
-	//soonchul add
-	//soonchul add2
-	//junggun add
-	//junggun add2
-	//soonchul add3
-	//gilim add
-	
-<<<<<<< HEAD
-	
-
-	/*
-	 * @RequestMapping(value = "/", method = RequestMethod.GET) // method명시 안하면
-	 * get,post 둘다 받음 public ModelAndView login() { mav = new ModelAndView();
-	 * mav.setViewName("loginFrm"); // 로그인 페이지로 이동
-	 * 
-	 * return mav; }
-	 */
-=======
-<<<<<<< HEAD
-	//sanggi add
-=======
->>>>>>> 277d1d63fce567148667e56fb0f9327a5a12f47c
->>>>>>> 32ab8de8163ab961f674186f034b9a3d2002b56f
 	
 }
