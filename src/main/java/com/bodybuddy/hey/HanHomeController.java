53ee4661259ba3ee3b61d7c10afc5d330cfc4411
steveKo <junggun24@naver.com>
2019-06-04 11:33:08 +0900

    sol

--- src/main/java/com/bodybuddy/hey/HanHomeController.java
+++ src/main/java/com/bodybuddy/hey/HanHomeController.java
@@ -14,8 +14,6 @@ import org.springframework.web.servlet.ModelAndView;
 import com.bodybuddy.hey.bean.Member;
 import com.bodybuddy.hey.service.MemberManagemant;
 
-
-
 //test12344
 /**
  * Handles requests for the application home page.
@@ -24,45 +22,51 @@ import com.bodybuddy.hey.service.MemberManagemant;
 
 public class HanHomeController {
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
-	@Autowired
-	private MemberManagemant mm;
+	
+	MemberManagemant mm ;
+
 	@Autowired
 	HttpSession session;
 
 	ModelAndView mav;
-	
+
 	@RequestMapping(value = "/login", method = RequestMethod.GET)
 	public String login(Model model) {
-	
+
 		return "loginJoinFrm/loginFrm";
 	}
+
 	@RequestMapping(value = "/join", method = RequestMethod.GET)
 	public String join(Model model) {
-		
+
 		return "loginJoinFrm/join";
 	}
+
 	@RequestMapping(value = "/nomaljoin", method = RequestMethod.GET)
 	public String nomaljoin(Model model) {
-		
+
 		return "loginJoinFrm/normalJoinFrm";
 	}
+
 	@RequestMapping(value = "/companyjoin", method = RequestMethod.GET)
 	public String companyjoin(Model model) {
-		
+
 		return "loginJoinFrm/companyJoinFrm";
 	}
+
 	@RequestMapping(value = "/trainerjoin", method = RequestMethod.GET)
 	public String trainerjoin(Model model) {
-		
+
 		return "loginJoinFrm/trainerJoinFrm";
 	}
+
 	@RequestMapping(value = "/nomalmemberjoin", method = RequestMethod.POST)
 	public ModelAndView nomalmemberjoin(Member mb) {
+
 		mav = mm.nomalmemberjoin(mb);
+		System.out.println(" 노말 조인 ");
 
 		return mav;
 	}
-	
-	
 
 }
--- src/main/java/com/bodybuddy/hey/service/MemberManagemant.java
+++ src/main/java/com/bodybuddy/hey/service/MemberManagemant.java
@@ -10,32 +10,30 @@ import org.springframework.web.servlet.ModelAndView;
 import com.bodybuddy.hey.bean.Member;
 import com.bodybuddy.hey.dao.MemberDao;
 
-@Service 
+@Service
 public class MemberManagemant {
-	@Autowired
+
+
 	private MemberDao mDao;
+
 	@Autowired
-	private HttpSession session; //request는 권장하지 않음
-	
+	private HttpSession session; // request는 권장하지 않음
+
 	ModelAndView mav;
 
 	public ModelAndView nomalmemberjoin(Member mb) {
 		mav = new ModelAndView();
-		String view = null;
-		//비번을 암호화(Encoding)할 수 있지만 복호화(Decoding)는 불가능
+		String view = null; // 비번을 암호화(Encoding)할 수 있지만 복호화(Decoding)는 불가능
 		BCryptPasswordEncoder pwdEncoder = new BCryptPasswordEncoder();
 		mb.setM_pw(pwdEncoder.encode(mb.getM_pw()));
-		
-		if(mDao.nomalmemberjoin(mb)) {
+
+		if (mDao.nomalmemberjoin(mb)) {
 			view = "loginFrm";
-			mav.addObject("check", 1); //회원가입 성공
-		}else {
-			view = "join";
+			
+			// 회원가입 성공 }else { view = "join"; } mav.setViewName(view);
+
 		}
-		mav.setViewName(view);
-		
 		return mav;
+
 	}
-
-
 }
