a7f3ccb21e6f46bb681ae2b303c2107196f46cad
gozldsos <https://github.com/gozldsos/gitHub.gitb.com>
2019-06-12 11:44:43 +0900

    yoon

--- src/main/java/com/bodybuddy/hey/YoonHomeController.java
+++ src/main/java/com/bodybuddy/hey/YoonHomeController.java
@@ -1,16 +1,19 @@
 
 package com.bodybuddy.hey;
 
+import java.io.IOException;
 import java.text.DateFormat;
 import java.util.Date;
 import java.util.Locale;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,6 +22,7 @@ import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.bodybuddy.hey.bean.Review;
 import com.bodybuddy.hey.service.YoonService;
 
 @Controller
@@ -47,14 +51,31 @@ public class YoonHomeController {
 	@RequestMapping(value = "/", method = {RequestMethod.POST,RequestMethod.GET})
 	public ModelAndView mainList() { //int pageNum 게시판페이징
 		
-		mav=ys.mainList();
+		HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes())
+				.getRequest();
+		mav=ys.mainList( request);
 		return mav;
 		
 	}
-	//manage/normal/normalMain
+	
 	@RequestMapping(value = "/infoprogramn")
 	public ModelAndView getInfoProgramN(String m_id) {
 		mav=ys.programListN(m_id);
+		return mav;
+	}
+	
+	
+	@RequestMapping(value = "/reviewwritefrm")
+	public ModelAndView reviewWriteFrm(String m_id, String ad_code)  {	
+		mav=ys.reviewWriteFrm(m_id, ad_code);
+		return mav;
+	}
+
+	
+	
+	@RequestMapping(value = "/reviewwriteinsert")
+	public ModelAndView insertReview(Review rv) {
+		mav=ys.insertReview(rv);
 		return mav;
 	}
 
@@ -64,9 +85,10 @@ public class YoonHomeController {
 		return mav;
 	}
 	@RequestMapping(value = "/dibsn")
-	public String login3(Model model) {
-		
-		return "manage/dibsListN";
+	public ModelAndView dibsList(String m_id) {
+		System.out.println("mmmmmmmmmmmmmmm="+m_id);
+		mav=ys.dibsList(m_id);
+		return mav;
 	}
 	@RequestMapping(value = "/memberdelten")
 	public String login4(Model model) {
---
+++ src/main/java/com/bodybuddy/hey/bean/Review.java
@@ -0,0 +1,21 @@
+package com.bodybuddy.hey.bean;
+
+import org.apache.ibatis.type.Alias;
+import org.springframework.lang.Nullable;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Alias("review")
+@Setter @Getter
+public class Review { 
+	private String rv_num;
+	private String rv_adcode;
+	private String rv_name;
+	private String rv_content;
+	private String rv_date;
+	private int rv_stpoint;
+	
+
+
+}
